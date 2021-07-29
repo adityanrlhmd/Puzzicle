dcd0b5a6e54dd78ed2046f58a8edb0afddab1c81
Aditya Nurul Hamda <adityanurulhamda@gmail.com>
2021-07-29 10:37:49 +0700

    update

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const toggler = mainNavigation.querySelector(".navbar-toggler");
 const togglerMenu = mainNavigation.querySelector(".icon-menu");
 const togglerClose = mainNavigation.querySelector(".icon-close");
 
+const sidebar = mainNavigation.querySelector(".navbar-nav");
+
 const openSideNav = () => mainNavigation.classList.add("active");
 const closeSideNav = () => mainNavigation.classList.remove("active");
 const iconMenu = () => togglerMenu.classList.add("active");
@@ -17,6 +19,10 @@ toggler.addEventListener("click", iconClose);
 overlay.addEventListener("click", closeSideNav);
 overlay.addEventListener("click", changeIconMenu);
 overlay.addEventListener("click", changeIconClose);
+
+sidebar.addEventListener("click", closeSideNav);
+sidebar.addEventListener("click", changeIconMenu);
+sidebar.addEventListener("click", changeIconClose);
 
 // document.addEventListener("swiped-right", openSideNav);
 document.addEventListener("swiped-left", closeSideNav);
